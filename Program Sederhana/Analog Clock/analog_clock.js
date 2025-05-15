b7acf083e6ce9868e6ea24b4fc2774058cf2e4af
= <=>
2025-05-15 10:43:02 +0700

    Program Sederhana

--- Program Sederhana/Analog Clock/analog_clock.js
+++ Program Sederhana/Analog Clock/analog_clock.js
@@ -5,9 +5,9 @@ const stalkHours = document.querySelector(".stalk-hours");
 setInterval(function () {
   const date = new Date();
 
-  const seconds = date.getSeconds() * 6; // 360° ÷ 60 detik = 6° per detik
-  const minutes = date.getMinutes() * 6 + date.getSeconds() * 0.1; // 6° per menit + 0.1° per detik untuk gerakan mulus
-  const hours = (date.getHours() % 12) * 30 + date.getMinutes() * 0.5; // 30° per jam + 0.5° per menit
+  const seconds = date.getSeconds() * 6; 
+  const minutes = date.getMinutes() * 6 + date.getSeconds() * 0.1; 
+  const hours = (date.getHours() % 12) * 30 + date.getMinutes() * 0.5; 
 
   stalkSeconds.style.transform = `rotate(${seconds}deg)`;
   stalkMinutes.style.transform = `rotate(${minutes}deg)`;
